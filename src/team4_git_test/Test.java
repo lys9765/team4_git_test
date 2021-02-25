355ab8acd264c42781ce33dec155d18ea92ef77d
HyunJay52 <hyunjungk52@gmail.com>
2021-02-25 09:28:10 +0900

    자바 테스트용 업데이트

---
+++ src/team4_git_test/Test.java
@@ -0,0 +1,14 @@
+package team4_git_test;
+
+public class Test {
+
+	public Test() {
+		// 각 자 브런치 생성 후, 소스를 추가해서 마스터에 머지해봅시다.
+		System.out.println("testing Git by TEAM 4");
+	}
+	
+	
+	public static void main(String[] arg) {
+		new Test();
+	}
+}
